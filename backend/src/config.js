08cafd7d72e92995a205140504f42536a6679a0a
muhammad arafat <80243954+markopat231@users.noreply.github.com>
2022-08-29 17:05:06 +0700

    config js

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x4b6a012d45BD0A601EE9c68e3cFb4A48Fc57C222"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = [0x4b6a012d45BD0A601EE9c68e3cFb4A48Fc57C222]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x4b6a012d45BD0A601EE9c68e3cFb4A48Fc57C222"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
